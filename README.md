eb682f71ef8970cfc4a2c7c90952a7d37255c356
Adrian Stanila <adrian.stanila@visma.com>
2013-02-20 23:54:07 +0200

    Fixing problem with --no- bug CHEF-2641 and switch to disable

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ patches
 Various Patches
 ---------------
 
-*   patch-vsphere_vm_clone_nocustomization.rb - knife-vsphere no customization patch
+*   patch-vsphere_vm_clone_disablecustomization.rb - knife-vsphere disable default customization patch
 
 
--- patch-vsphere_vm_clone_disablecustomization.rb
+++ patch-vsphere_vm_clone_disablecustomization.rb
@@ -1,17 +1,19 @@
---- vsphere_vm_clone.rb	2013-02-20 21:57:38.204080139 +0200
-+++ vsphere_vm_clone_nocustomization.rb	2013-02-20 22:00:23.247082756 +0200
-@@ -158,6 +158,10 @@
- 		:long => "--no-host-key-verify",
+--- vsphere_vm_clone.rb	2013-02-20 23:51:57.534726802 +0200
++++ vsphere_vm_clone_disablecustomization.rb	2013-02-20 23:52:24.781874436 +0200
+@@ -159,6 +159,12 @@
  		:description => "Disable host key verification",
  		:boolean => true
-+	option :no_customization,
-+		:long => "--no-customization",
-+		:description => "Disable default customization",
-+		:boolean => false
  
++	option :disable_customization,
++		:long => "--disable-customization",
++		:description => "Disable default customization",
++		:boolean => true,
++		:default => false
++
  	def run
  		$stdout.sync = true
-@@ -231,9 +235,7 @@
+ 
+@@ -231,9 +237,7 @@
  			rspec.datastore = find_datastore(get_config(:datastore))
  		end
  
@@ -22,15 +24,15 @@
  
  		clone_spec.config = RbVmomi::VIM.VirtualMachineConfigSpec(:deviceChange => Array.new)
  
-@@ -289,6 +291,7 @@
+@@ -289,6 +293,7 @@
  			end
  		end
  
-+		if get_config(:no_customization)
++		if !get_config(:disable_customization)
  		use_ident = !config[:customization_hostname].nil? || !get_config(:customization_domain).nil? || cust_spec.identity.nil?
  
  		if use_ident
-@@ -312,6 +315,8 @@
+@@ -312,6 +317,8 @@
  		end
  
  		clone_spec.customization = cust_spec
